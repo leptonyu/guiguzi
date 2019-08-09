b37810aa8e00052a2ba28a0500ca98a01cd0b77b
Daniel YU <i@icymint.me>
2019-08-09 14:51:24 +0800

    add captcha

--- captcha/src/Data/Captcha.hs
+++ captcha/src/Data/Captcha.hs
@@ -1,3 +1,15 @@
 module Data.Captcha where
 
-main = putStrLn "Hello World"
+import Servant
+import Servant.Server.Internal
+import Servant.Swagger
+
+data CheckCaptcha
+
+instance HasServer api context => HasServer (CheckCaptcha :> api) context where
+  type ServerT (CheckCaptcha :> api) m = ServerT api m
+  route _ = route (Proxy @api)
+  hoistServerWithContext _ = hoistServerWithContext (Proxy @api)
+
+instance HasSwagger api => HasSwagger (CheckCaptcha :> api) where
+  toSwagger _ = toSwagger (Proxy @api)
